290018b1d7db79aeae1fcf9dce4da268ec188940
kraftonius <craftyknight@gmail.com>
2023-03-16 01:37:12 +0300

    add example002

--- Example002_HelloUser/obj/Debug/net6.0/Example002_HelloUser.AssemblyInfo.cs
+++ Example002_HelloUser/obj/Debug/net6.0/Example002_HelloUser.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Example001_HelloConsole")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Example002_HelloUser")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Example001_HelloConsole")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Example001_HelloConsole")]
+[assembly: System.Reflection.AssemblyProductAttribute("Example002_HelloUser")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Example002_HelloUser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
