9b83d3eab94a4cc444542bb0501cd5368b1b16b8
kraftonius <craftyknight@gmail.com>
2023-03-16 04:27:58 +0300

    Task 00 finished

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Напишите программу, которая
+1. на вход принимает число 
+2. выдает квадрат (число умноженное на само себя)
+*/
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.WriteLine($"Квадрат числа {number} = {square}");
