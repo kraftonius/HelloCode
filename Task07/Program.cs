1257b2d20b7e3cb8130cc4844b6495c0beeda1d4
kraftonius <craftyknight@gmail.com>
2023-03-16 05:36:41 +0300

    finished task 07

--- Task07/Program.cs
+++ Task07/Program.cs
@@ -10,10 +10,16 @@
 Console.WriteLine("Введите трезначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int lastDigit = 0;
+
+if (number < 0)
+{
+number = - number;
+}
+
 if (number <= 999 && number >= 100)
 {
     lastDigit = number % 10;
-     Console.WriteLine($"Последняя цифра числа {number}: {lastDigit} ");
+    Console.WriteLine($"Последняя цифра числа {number}: {lastDigit} ");
 }
 else
 {
