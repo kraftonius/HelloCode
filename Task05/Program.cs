f92878a14999366e43b347bbf1beb409c5578f09
kraftonius <craftyknight@gmail.com>
2023-03-16 05:20:52 +0300

    finished task 05

---
+++ Task05/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Напишите программу, которая
+1. На вход принимает одно число N,
+2. На выходе показывает все числа в промежутке от -N до N
+4 -> "-4,-3,-2,-1,0,1,2,3,4"
+2 -> "-2,-1,0,1,2"
+*/
+Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >0)
+{
+    int index = -number;
+    while (index <= number)
+    {
+        Console.Write($"{index}, ");
+        index++;
+    }
+}
+else
+{
+    Console.WriteLine("Введено ненатуральное число");
+}
