6df1183f8e3b53072a0c26ebb388a2dc6a3a24ff
kraftonius <craftyknight@gmail.com>
2023-03-16 04:46:58 +0300

    add task 03

---
+++ Task03/Program.cs
@@ -0,0 +1,13 @@
+﻿// По заданному номеру дня недели вывести его название
+
+Console.WriteLine("Введите номер дня недели от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 1) Console.Write("Понедельник");
+else if (number == 2) Console.Write("Вторник");
+else if (number == 3) Console.Write("Среда");
+else if (number == 4) Console.Write("Четверг");
+else if (number == 5) Console.Write("Пятница");
+else if (number == 6) Console.Write("Суббота");
+else if (number == 7) Console.Write("Воскресение");
+else Console.Write("Введен неверный номер. Необходимо ввести от 1 до 7!");
