f8c878bfecede137e72a66e69ef5ff07f57a4e02
kraftonius <craftyknight@gmail.com>
2023-03-16 06:26:27 +0300

    finish Task08

---
+++ Task08_homework/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int index = 2;
+if (number == 1) Console.WriteLine("Нет четных чисел от 1 до 1");
+else if (number >0)
+{
+    while (index <= number)
+    {
+        Console.Write($"{index} ");
+        index = index + 2;
+    }
+}
+else
+{
+    Console.WriteLine("Введено ненатуральное число");
+}
