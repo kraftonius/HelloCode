76cc98adbf211f773212af174cf35d5372ad7bad
kraftonius <craftyknight@gmail.com>
2023-03-16 04:40:31 +0300

    add task 01

---
+++ Task01/Program.cs
@@ -0,0 +1,16 @@
+﻿/* 
+По двум заданным числам проверять является ли первое квадратом второго
+*/
+
+Console.WriteLine("Введите первое целое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2 * number2)
+{
+    Console.WriteLine($"{number1} является квадратом {number2}");
+}
+else
+{
+    Console.WriteLine($"{number1} не является квадратом {number2}");
+}
