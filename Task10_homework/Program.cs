3a227d72f2900e97b2b05bca4bd21c319017a222
kraftonius <craftyknight@gmail.com>
2023-03-17 03:57:13 +0300

    added Task10

---
+++ Task10_homework/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 10: 
+// Напишите программу, которая
+// 1. принимает на вход трёхзначное число
+// 2. и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(IsThreeDigits(number)?GetSecondDigit(number):"Не является трехзначным числом" );
+
+
+// Функция проверки на трехзначное число. Отрицательные трехзначные тоже считаем трехзначными.
+bool IsThreeDigits(int num)
+{
+    return Math.Abs(num) >= 100 && Math.Abs(num) <= 999;
+}
+
+
+// Функция возвращения второй цифры трехзначного числа
+int GetSecondDigit(int numb)
+{
+    return Math.Abs(numb) / 10 % 10;
+}
