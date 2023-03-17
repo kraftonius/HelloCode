9f6a85bc5b9a8bad4326c605ef759d2bc8dceaad
kraftonius <craftyknight@gmail.com>
2023-03-17 03:08:01 +0300

    add Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,21 @@
+﻿// 14. Напишите программу, которая
+// 1. принимает на вход число
+// 2. и проверяет, кратно ли оно одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+int multi1 = 7;
+int multi2 = 23;
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string print = isMulti(number, multi1, multi2) ? "Да" : "Нет";
+Console.WriteLine(print);
+
+
+bool isMulti (int num, int mul1, int mul2)
+{
+    return num % mul1 == 0 && num % mul2 == 0; 
+}
