b91e39553bc08cf75db80abaaaa35efd86bc86cc
kraftonius <craftyknight@gmail.com>
2023-03-18 08:28:56 +0300

    Task 20 and Task 22

---
+++ Task20/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 20: Напишите программу, которая
+// принимает на вход координаты двух точек и
+// находит расстояние между ними в 2D
+// пространстве.
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+
+Console.WriteLine($"Введите координаты 1-й точки");
+Console.Write("X1: ");
+int xCoord1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y1: ");
+int yCoord1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Введите координаты 2-й точки");
+Console.Write("X2: ");
+int xCoord2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y2: ");
+int yCoord2 = Convert.ToInt32(Console.ReadLine());
+
+double res = GetLength(xCoord1, yCoord1, xCoord2, yCoord2);
+double resRound = Math.Round(res, 2, MidpointRounding.ToZero);
+
+Console.WriteLine(resRound);
+
+
+double GetLength(int x1, int y1, int x2, int y2)
+{
+    return Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+}
---
+++ Task22/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача22:
+// Напишите программу, которая
+// 1. принимает на вход число (N)
+// 2. и выдает таблицу квадратов числел от 1 до N
+// 5 -> 1, 3, 9, 16, 25.
+// 2 -> 1, 4
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void SquaresTable(int num)
+{
+    if (num > 0)
+    {
+        int index = 1;
+        while (index <= num)
+        {
+            Console.WriteLine($"{index, 5} -> {index * index, 5}");
+            index++;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Введено ненатуральное число");
+    }
+}
+
+SquaresTable(number);
+
+
