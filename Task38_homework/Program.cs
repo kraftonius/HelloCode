9c6a61080d28dd1b56c5cface5ae0f0e80d0b757
kraftonius <craftyknight@gmail.com>
2023-03-20 22:27:50 +0300

    add Task38

---
+++ Task38_homework/Program.cs
@@ -0,0 +1,24 @@
+﻿Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+[3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+
+
+
+
+
+
+
+
+
+int[] GenerateArray(int size, int min, int max) // Создает массив size - кол-во элементов. min и max - диапазон значений элемента
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
