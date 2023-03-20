71c1eeef7b4994317443ff4c9a187157146b511c
kraftonius <craftyknight@gmail.com>
2023-03-20 06:24:32 +0300

    Task31, Task32, Task33

---
+++ Task32/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 32: Напишите программу замена элементов
+// массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string ReadInput = Console.ReadLine();
+    int result = Convert.ToInt32(ReadInput);
+    return result;
+}
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+int[] ChangeSign(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = -arr[i];
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i], 3}, ");
+        else Console.Write($"{arr[i], 3}");
+    }
+    Console.WriteLine("]");
+}
+
+int size = Prompt("Длина Массива: ");
+int min = Prompt("Минимальное значение случайного числа: ");
+int max = Prompt("Максимальное значение случайного числа: ");
+int[] array = CreateArrayRndInt(size, min, max);
+PrintArray(array);
+int[] changedSignsArray = ChangeSign(array);
+PrintArray(changedSignsArray);
---
+++ Task33/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 33:  Напишите программу, которая
+// 1. Задайте массив.
+// 2. определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string ReadInput = Console.ReadLine();
+    int result = Convert.ToInt32(ReadInput);
+    return result;
+}
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+bool HasNumber(int num, int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == num) return true;
+    }
+    return false;
+}
+
+string arrayToPrint(int[] arr)
+{
+    string printArray = String.Empty;
+    printArray += "[";
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) printArray += $"{arr[i], 2}, ";
+        else printArray += $"{arr[i], 2}";
+    }
+    printArray += "]";
+    return printArray;
+}
+
+int[] array = CreateArrayRndInt(10, 0, 9);
+Console.WriteLine(arrayToPrint(array));
+int number = Prompt("Число для поиска: ");
+string hasNumber = HasNumber(number, array) ? "да" : "нет";
+
+Console.WriteLine($"{number}; массив {arrayToPrint(array)} -> {hasNumber}");
