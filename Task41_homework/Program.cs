851a3bb0bf958cc260999bbb4cf1c7837d876d36
kraftonius <craftyknight@gmail.com>
2023-03-22 17:30:42 +0300

    homework: Task41, Task43

---
+++ Task41_homework/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] PromptArray(string message)
+{
+    Console.Write(message);
+    string ReadInput = Console.ReadLine();
+    int num = Convert.ToInt32(ReadInput);
+    int[] arr = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        Console.Write($"Введите число {i+1} из {num}: ");
+        string ReadInput2 = Console.ReadLine();
+        arr[i] = Convert.ToInt32(ReadInput2);
+    }
+    return arr;
+}
+
+int CountPositive(int[] arr)
+{
+    int quantity = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) quantity++;
+    }
+    return quantity;
+}
+
+string IntArrayStringView(int[] arr)
+// Возвращает массив целых чисел внутри квадратных скобок через запятую
+// [34, 26, 76, 88, 45,  8, 62]
+{
+    string printArray = "[";
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) printArray += $"{arr[i],2}, ";
+        else printArray += $"{arr[i],2}]";
+    }
+    return printArray;
+}
+
+int[] array = PromptArray("Введите количество чисел для ввода: ");
+int countPositive = CountPositive(array);
+Console.WriteLine($"{IntArrayStringView(array)} -> {countPositive}");
---
+++ Task43_homework/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 43: Напишите программу, которая 
+// 1. найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// 2. значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// y = k1 * x + b1, y = k2 * x + b2;
+// for x
+// k2x + b2 = k1x + b1; 
+// k2x - k1x = b1 - b2;
+// x = (b1 - b2) / (k2 - k1);
+// for y
+// y = k1x + b1;
+// y = k1((b1 - b2) / (k2 - k1)) + b1;
+// y = ( k1((b1 - b2) + b1(k2 - k1) ) / (k2 - k1);
+// y = ( k1b1 - k1b2 + k2b1 - k1b1) / (k2 - k1);
+// y = (k1b2 - k2b1) / (k1 - k2);
+
+double PromptDouble(string message)
+{
+    Console.Write(message);
+    string ReadInput = Console.ReadLine();
+    double result = Convert.ToDouble(ReadInput);
+    return result;
+}
+
+double GetXCoord(double b1, double k1, double b2, double k2)
+{
+    return (b1 - b2) / (k2 - k1);
+}
+
+double GetYCoord(double b1, double k1, double b2, double k2)
+{
+    return ((k1 * b2 - k2 * b1) / (k1 - k2));
+}
+
+Console.WriteLine("Для уравнений: y = k1 * x + b1, y = k2 * x + b2 введите значения");
+double b1Value = PromptDouble("Введите значение b1: ");
+double k1Value = PromptDouble("Введите значение k1: ");
+double b2Value = PromptDouble("Введите значение b2: ");
+double k2Value = PromptDouble("Введите значение k2: ");
+double xCoord = GetXCoord(b1: b1Value, k1: k1Value, b2: b2Value, k2: k2Value);
+double yCoord = GetYCoord(b1: b1Value, k1: k1Value, b2: b2Value, k2: k2Value);
+Console.WriteLine($"b1 = {b1Value}, k1 = {k1Value}, b2 = {b2Value}, k2 = {k2Value} -> ({xCoord}; {yCoord})");
