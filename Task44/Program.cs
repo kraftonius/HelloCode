345955f828974d956b0335b1d5dcf2186e57ec4e
kraftonius <craftyknight@gmail.com>
2023-03-22 15:48:58 +0300

    Task 44, 45

---
+++ Task44/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел
+// Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+int[] FibonacciArray(int num)
+{
+    int[] arr = new int[num];
+    arr[0] = 0;
+    arr[1] = 1;
+    for (int i = 2; i < num; i++)
+    {
+        arr[i] = arr[i-2] + arr[i-1];
+    }
+    return arr;
+}
+
+void PrintArraySplitSpace(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");       
+    }
+    Console.WriteLine(); 
+}
+
+int number = 20;
+int[] fibonacciArray = FibonacciArray(number);
+Console.Write($"Если N = {number} -> ");
+PrintArraySplitSpace(fibonacciArray);
---
+++ Task45/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 45: Напишите программу, которая будет создавать
+// копию заданного массива с помощью поэлементного
+// копирования.
+
+
+int[] GenerateRndArray(int size, int min, int max)
+// Создает массив целых чисел
+// size - кол-во элементов. min и max - диапазон значений элементов
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+int[] CopyArray(int[] arr)
+{
+    int[] copyArr = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        copyArr[i] = arr[i];
+    }
+    return copyArr;
+}
+
+string IntArrayStringView(int[] arr)
+// Возвращает массив целых чисел внутри квадратных скобок через запятую
+// [34, 26, 76, 88, 45,  8, 62]
+{
+    string printArray = "[";
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) printArray += $"{arr[i],2}, ";
+        else printArray += $"{arr[i],2}]";
+    }
+    return printArray;
+}
+
+int[] array = GenerateRndArray(5, 1, 9);
+int[] copyArray = CopyArray(array);
+Console.WriteLine($"{IntArrayStringView(array)} -> {IntArrayStringView(copyArray)}");
